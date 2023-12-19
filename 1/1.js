2898ce44ac868f3ef2465f054b6ff0be292879d4
Maxim Volkov <mv_volkov@eureca.ru>
2023-12-19 10:30:18 +0300

    Unify tabs size in js files

--- 1/1.js
+++ 1/1.js
@@ -9,41 +9,41 @@ var words = ['Портфолио по курсу "Верстки" Волкова
     speed = 70;
 
 var wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
+    setInterval(function () {
+        if (forwards) {
+            if (offset >= words[i].length) {
+                    ++skip_count;
 
-        if (skip_count == skip_delay) {
-          forwards = false;
-          skip_count = 0;
+                if (skip_count == skip_delay) {
+                    forwards = false;
+                    skip_count = 0;
+                }
+            }
         }
-      }
-    }
-    else {
-      if (offset == 0) {
-        forwards = true;
-        i++;
-        offset = 0;
+        else {
+            if (offset == 0) {
+                forwards = true;
+                i++;
+                offset = 0;
 
-        if (i >= len)
-          i = 0;
-      }
-    }
+                if (i >= len)
+                    i = 0;
+            }
+        }
 
-    part = words[i].substr(0, offset);
+        part = words[i].substr(0, offset);
 
-    if (skip_count == 0) {
-      if (forwards)
-        offset++;
-      else
-        offset--;
-    }
+        if (skip_count == 0) {
+            if (forwards)
+                offset++;
+            else
+                offset--;
+        }
 
-    $('.greeting').text(part);
-  },speed);
+        $('.greeting').text(part);
+    },speed);
 };
 
 $(document).ready(function () {
-  wordflick();
+    wordflick();
 });
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function setActiveTab(id) {
-  // Get current active tab and deactivate it
-  var menu = document.getElementById('menu');
-  var activeTab = menu.children[0];
+	// Get current active tab and deactivate it
+	var menu = document.getElementById('menu');
+	var activeTab = menu.children[0];
 
-  for (const tab of menu.children) {
-    if (tab.classList.contains('active')) {
-      activeTab = tab;
-      break;
-    }
-  }
+	for (const tab of menu.children) {
+		if (tab.classList.contains('active')) {
+			activeTab = tab;
+	  		break;
+		}
+  	}
 
-  activeTab.classList.remove('active');
+  	activeTab.classList.remove('active');
 
-  // Make selected tab active
-  document.getElementById(id).classList.add('active');
+	// Make selected tab active
+	document.getElementById(id).classList.add('active');
 
-  // Load selected tab html
-  document.getElementsByTagName('iframe')[0].src = `${id}/${id}.html`;
+	// Load selected tab html
+	document.getElementsByTagName('iframe')[0].src = `${id}/${id}.html`;
 }
 
 $(document).ready(function () {
-  setActiveTab('1');
+  	setActiveTab('1');
 });
