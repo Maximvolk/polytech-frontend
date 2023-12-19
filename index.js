8e9484f0e067ce2abe4d2ff633f73200b47bd8b7
Maxim Volkov <mv_volkov@eureca.ru>
2023-12-19 09:18:52 +0300

    Implement module structure based on iframe

--- index.js
+++ index.js
@@ -1,53 +1,24 @@
-var words = ['Портфолио по курсу "Верстки" Волкова Максима'],
-    part,
-    i = 0,
-    offset = 0,
-    len = words.length,
-    forwards = true,
-    skip_count = 0,
-    skip_delay = 15,
-    speed = 70;
+function setActiveTab(id) {
+  // Get current active tab and deactivate it
+  var menu = document.getElementById('menu');
+  var activeTab = menu.children[0];
 
-var wordflick = function () {
-  setInterval(function () {
-    if (forwards) {
-      if (offset >= words[i].length) {
-        ++skip_count;
-
-        if (skip_count == skip_delay) {
-          forwards = false;
-          skip_count = 0;
-        }
-      }
+  for (const tab of menu.children) {
+    if (tab.classList.contains('active')) {
+      activeTab = tab;
+      break;
     }
-    else {
-      if (offset == 0) {
-        forwards = true;
-        i++;
-        offset = 0;
+  }
 
-        if (i >= len) {
-          i = 0;
-        }
-      }
-    }
+  activeTab.classList.remove('active');
 
-    part = words[i].substr(0, offset);
+  // Make selected tab active
+  document.getElementById(id).classList.add('active');
 
-    if (skip_count == 0) {
-      if (forwards) {
-        offset++;
-      }
-      else {
-        offset--;
-      }
-    }
-
-    $('.content').text(part);
-    console.log(part);
-  },speed);
-};
+  // Load selected tab html
+  document.getElementsByTagName('iframe')[0].src = `${id}/${id}.html`;
+}
 
 $(document).ready(function () {
-  wordflick();
+  setActiveTab('1');
 });
